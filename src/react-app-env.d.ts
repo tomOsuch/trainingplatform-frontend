8af9c3e0bc0bc6964ff460ca1d3ca92eb0988aba
Tomasz Osuch <tomasz.osuch@gmail.com>
2026-08-07 00:28:42 +0200

    git commit -m "first commit"

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+// Globalne (nie-modułowe) arkusze stylów importowane dla efektu ubocznego.
+// CRA deklaruje tylko *.module.scss, więc zwykłe .scss/.css dopisujemy sami.
+declare module "*.scss";
+declare module "*.sass";
+declare module "*.css";
